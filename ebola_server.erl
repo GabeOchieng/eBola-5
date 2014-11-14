ddbacdf6388573b63e9ffbab216c23e8fd7646c9
Broratheon <hyungseo0107@gmail.com>
2014-11-14 10:12:20 -0500

    Added coordinates for patients

--- ebola_server.erl
+++ ebola_server.erl
@@ -3,15 +3,16 @@
 
 % Creates a list of Patient PIDs and spawns the server loop.
 % Takes in number of patients, a list of names and a list of their current health status
-start(NumPatients, Names, Health) ->
-	Patients = make_patients(NumPatients, Names, Health),
+start(NumPatients, Names, Health, Coordinates) ->
+	Patients = make_patients(NumPatients, Names, Health, Coordinates),
 	Server = spawn(ebola_server, loop, [Patients]),
 	send_server_to_patients(Patients, Server).
 
-% Creates a list of PIDs of the patients.
-make_patients(0, _X, _Y) -> [];
-make_patients(NumPatients, [A | B], [C | D]) -> 
-		[spawn(patient, start, [{A, C}]) | make_patients(NumPatients - 1, B, D)].
+% Creates a list of tuple of {PIDs, Coordinate} of the patients.
+make_patients(0, _X, _Y, _Z) -> [];
+make_patients(NumPatients, [A | B], [C | D], [E | F]) -> 
+		% Note: Patient himself doesn't need to know his location. Server deals with that.
+		[ {spawn(patient, start, [{A, C}]), E} | make_patients(NumPatients - 1, B, D, F)].
 
 % Server loop.
 loop(Patients) ->
@@ -30,8 +31,8 @@ print_patient_state(Name, Health) ->
 	Msg = string:concat(string:concat(Name, " is "), Health),
 	io:fwrite(string:concat(Msg, "~n")).
 
-send_server_to_patients([A | []], Server) -> A ! {server, Server};
-send_server_to_patients([A | B], Server) -> A ! {server, Server}, send_server_to_patients(B, Server).
+send_server_to_patients([{PID, _} | []], Server) -> PID ! {server, Server};
+send_server_to_patients([{PID, _} | B], Server) -> PID ! {server, Server}, send_server_to_patients(B, Server).
 
 % Send a 'print' message to all the patients.
 print_all_patients([A | [] ]) -> A ! print;
