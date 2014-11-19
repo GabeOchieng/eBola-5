5e55962c998516571ef9baa9b2027fd9c8884204
robruenes <robruenes@gmail.com>
2014-11-19 17:08:54 -0500

    adding basic run foo

--- ebola.py
+++ ebola.py
@@ -2,7 +2,10 @@ from erlport.erlterms import Atom
 from erlport.erlang import call, cast, set_message_handler
 
 def run():
-    
-
+    Names = ["Harry", "FuckFace", "ShitEater", "DumbFuckingFuck"]
+    Health = [Atom("clean"), Atom("dormant"), Atom("clean"), Atom("clean")]
+    Coords = [(0, 0), (1, 0), (0, 1), (1, 1)]
+    DiseaseParams = (5, .5)
+    ServerPID = call(Atom("ebola_server"), Atom("start"), [Names, Health, Coords, DiseaseParams])
 
 run()
