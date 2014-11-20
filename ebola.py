b3c8724b7e5da55d844c2dc8ac3209f7b3666744
robruenes <robruenes@gmail.com>
2014-11-19 23:28:03 -0500

    adding version that shows communication between the two, and that has the python module write a messages file to disk showing the state change messages

--- ebola.py
+++ ebola.py
@@ -1,7 +1,13 @@
 from erlport.erlterms import Atom
 from erlport.erlang import call, cast, set_message_handler
 
+def handler(message):
+
+    f = open('messages', 'a')
+    f.write(str(message))
+
 def run(ServerPID):
+    set_message_handler(handler)
     Names = ["Harry", "FuckFace", "ShitEater", "DumbFuckingFuck"]
     Health = [Atom("clean"), Atom("dormant"), Atom("clean"), Atom("clean")]
     Coords = [(0, 0), (1, 0), (0, 1), (1, 1)]
--- ebola_server.erl
+++ ebola_server.erl
@@ -1,13 +1,14 @@
 -module(ebola_server).
--export([start_simulation/0, start/4, loop/1, print_all_patients/1]).
+-export([start_simulation/0, start/5, loop/2, print_all_patients/1]).
 
 % Creates a list of Patient PIDs and spawns the server loop.
 % Takes in number of patients, a list of names and a list of their current health status
 % Coordinates is a tuple of {X, Y}.
-start(Names, Health, Coordinates, {Tick_time, Disease_Strength}) ->
+start(PythonInstance, Names, Health, Coordinates, {Tick_time, Disease_Strength}) ->
 	Patients = make_patients(Names, Health, Coordinates, Tick_time, Disease_Strength),
-	Server = spawn(ebola_server, loop, [Patients]),
-	send_server_to_patients(Patients, Server).
+	Server = spawn(ebola_server, loop, [PythonInstance, Patients]),
+	send_server_to_patients(Patients, Server),
+	test_infect_patients(Patients).
 
 % Creates a list of tuple of {PIDs, Coordinate} of the patients.
 make_patients([], [], [], _, _) -> [];
@@ -17,19 +18,22 @@ make_patients([Name | NTail], [Health | HTail], [Coord | CTail], Tick_time, Dise
 
 is_neighbor({X, Y}, {X2, Y2}) -> (abs(X2 - X) =< 1) and (abs(Y2 - Y) =< 1).	
 
+test_infect_patients([{PID, _} | []]) -> PID ! infect;
+test_infect_patients([{PID, _} | Tail]) -> PID ! infect, test_infect_patients(Tail).
+
 % Server loop.
-loop(Patients) ->
+loop(PythonInstance, Patients) ->
 	 %print_all_patients(Patients),
 	 %timer:apply_after(5000, ebola_server, loop, [Patients]).
 
 	receive
-		{state_change, Name, Health} -> print_patient_state(Name, Health); % Produce new Patients list with changed state.
+		{state_change, Name, Health} -> python:cast(PythonInstance, {state_change, Name, Health}), print_patient_state(Name, Health); % Produce new Patients list with changed state.
 		{spread, PID, Health} 		 -> find_coord(Patients, PID, Health, Patients) % Need to find neighbors here.
 		% true 			 	-> print_patient_state("Fuckface", "sick")
 	after 0      			-> timeout
 	end,
 
-	loop(Patients).
+	loop(PythonInstance, Patients).
 
 %%This is very broken, not sure why yet. Full version ends up exiting with an error.
 print_patient_state(_Name, _Health) -> io:fwrite("Hello").
@@ -79,9 +83,9 @@ start_simulation() ->
     python:call(P, ebola, run, [self()]),
     wait_for_settings(P).
 
-wait_for_settings(_PythonInstance) ->
+wait_for_settings(PythonInstance) ->
 	receive
-		{initial_settings, Names, Health, Coordinates, {Tick_time, Disease_Strength}} -> start(Names, Health, Coordinates, {Tick_time, Disease_Strength});
+		{initial_settings, Names, Health, Coordinates, {Tick_time, Disease_Strength}} -> start(PythonInstance, Names, Health, Coordinates, {Tick_time, Disease_Strength});
 		_ -> io:fwrite("Got a message")
 
 	end.
